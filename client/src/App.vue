339e2c93c00b3b7e5d6595f0c873c29767d9efcf
Justin Coker <justin.coker@1159.ai>
2024-08-09 20:37:48 -0400

    moved authentication to app.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,26 @@
 <template>
-  <router-view />
+  <router-view v-if="authStatus === 'authenticated'" />
+  <q-layout view="hHh lpR fFf" v-else>
+    <q-page-container>
+      <q-page class="flex-center row bg-white">
+        <authenticator :hide-sign-up="true">
+          <template v-slot:sign-in-header>
+            <div class="flex-center row q-pt-lg">
+            <img src="/1159-teal-gradient.png" :style="{maxWidth: '60%', height: 'auto'}" />
+          </div>
+          </template>
+        </authenticator> </q-page
+      >>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script setup lang="ts">
+import { toRefs } from 'vue';
+import { Authenticator, useAuthenticator } from '@aws-amplify/ui-vue';
+import '@aws-amplify/ui-vue/styles.css';
+const { authStatus } = toRefs(useAuthenticator());
 defineOptions({
-  name: 'App'
+  name: 'KbDemo',
 });
 </script>
