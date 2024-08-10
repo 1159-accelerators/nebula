606dda680c5704674d82553736df8037d2733f21
Justin Coker <justin.coker@1159.ai>
2024-08-09 20:26:37 -0400

    added error handler to axios

--- client/src/boot/axios.ts
+++ client/src/boot/axios.ts
@@ -1,5 +1,6 @@
 import { boot } from 'quasar/wrappers';
 import axios, { AxiosInstance } from 'axios';
+import { Notify } from 'quasar';
 
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
@@ -9,28 +10,31 @@ declare module '@vue/runtime-core' {
 }
 
 const getBaseUrl = async () => {
-  const { baseUrl } = await fetch('/config.json').then((response) => response.json())
-  return baseUrl
-}
-// Be careful when using SSR for cross-request state pollution
-// due to creating a Singleton instance here;
-// If any client changes this (global) instance, it might be a
-// good idea to move this instance creation inside of the
-// "export default () => {}" function below (which runs individually
-// for each client)
+  const { baseUrl } = await fetch('/config.json').then((response) =>
+    response.json()
+  );
+  return baseUrl;
+};
+
 const api = axios.create();
 
 export default boot(async ({ app }) => {
-  // for use inside Vue files (Options API) through this.$axios and this.$api
-
   app.config.globalProperties.$axios = axios;
-  // ^ ^ ^ this will allow you to use this.$axios (for Vue Options API form)
-  //       so you won't necessarily have to import axios in each vue file
 
   app.config.globalProperties.$api = api;
-  // ^ ^ ^ this will allow you to use this.$api (for Vue Options API form)
-  //       so you can easily perform requests against your app's API
-  api.defaults.baseURL = await getBaseUrl()
+
+  api.defaults.baseURL = await getBaseUrl();
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      Notify.create({
+        type: 'negative',
+        position: 'top',
+        message: 'Something went wrong',
+      });
+      console.log(error);
+    }
+  );
 });
 
 export { api };
