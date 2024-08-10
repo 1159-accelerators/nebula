891cf43e2544281b9fcd626aa263dd58781e8828
Justin Coker <justin.coker@1159.ai>
2024-08-09 20:24:19 -0400

    moved auth to pinia store

---
+++ client/src/stores/auth-store.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+//import { toRefs } from 'vue';
+import { useAuthenticator } from '@aws-amplify/ui-vue';
+import { fetchAuthSession } from 'aws-amplify/auth';
+
+const authenticator = useAuthenticator();
+
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    idToken: '' as string | undefined,
+    authStatus: authenticator.authStatus
+  }),
+  // getters: {
+  //   doubleCount: (state) => state.counter * 2,
+  // },
+  actions: {
+    signOut() {
+      authenticator.signOut();
+    },
+    async setToken() {
+      try {
+        const session = await fetchAuthSession()
+        this.idToken = session.tokens?.idToken?.toString()
+      } catch(err) {
+        console.log(err)
+      }
+    }
+  }
+  },
+);
